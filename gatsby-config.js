70d6ac8f8e967c2121ae8da2f67762b71b92d6cb
Benjamin Leroux <belero@microsoft.com>
2021-03-29 21:38:20 -0400

    Changed google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-180300326-1",
+        trackingId: "UA-193367479-1",
       },
     },
     `gatsby-plugin-react-helmet`,
