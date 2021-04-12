33402cebcc32f4bb7c4c878b4cee344261020145
Benjamin Leroux <belero@microsoft.com>
2021-04-12 15:34:51 -0400

    fixed graphQL query error

--- gatsby-config.js
+++ gatsby-config.js
@@ -201,7 +201,6 @@ module.exports = {
                 ) {
                   edges {
                     node {
-                      html
                       fields { 
                         slug
                       }
